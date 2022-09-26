7a0ea1603757d84056c54785463b3292104d0ae0
Er3en <minexix@gmail.com>
2022-09-26 23:31:36 +0200

    change ds

---
+++ tests/test_dataset.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+import unittest
+from hydra import compose, initialize
+from omegaconf import OmegaConf
+from src.dataset.dataset import ClassificationDataset
+
+####TODO###
+class TestDataset(TestCase):
+
+    def setUp(self):
+        cfg = OmegaConf.load("cfg/config.yaml")
+        self.path = cfg.dataset.path
+
+
+    def test_data_labels(self):
+        dataloader = ClassificationDataset(self.path)
+        df_data = dataloader._create_df()
+        df_data.label.unique()
+        
--- train.py
+++ train.py
@@ -1,14 +1,14 @@
 import torch
 import hydra
 import numpy as np
-from src.dataloader import *
+from src.dataset import *
 from src.utils import *
 
 @hydra.main(config_path="cfg", config_name="config")
 def main(cfg):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"INFO: device {device}")   
-    train_dataset = ClassificationDataset(cfg.path)
+    train_dataset = ClassificationDataset(cfg.dataset.path)
 
    
 
